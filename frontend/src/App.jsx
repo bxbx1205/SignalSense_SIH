438f05fd2ee7e58d8d024623b20eacc6ed0c9c85
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2025-09-16 20:30:41 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.jsx 	renamed:    frontend/src/components/DarkVeil .jsx -> frontend/src/components/DarkVeil.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import TrainControlViewer from './components/Controllers/TrainControlViewer.jsx'
 import TrainControlOperator from './components/Controllers/TrainControlOperator.jsx'
+import DarkVeil from './components/DarkVeil.jsx'
 
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
 
   return (
     <>
+    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <DarkVeil />
+</div>
      <TrainControlViewer />
      {/* <p>alag</p> */}
      <TrainControlOperator />
